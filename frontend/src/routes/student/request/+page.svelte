9106bf8e8ade32fae04590a2e22d0c3d866cfd4d
Jharana Sapkota <jharanasapkota09@gmail.com>
2024-06-18 20:40:01 +0545

    Remove course selection on request

--- frontend/src/routes/student/request/+page.svelte
+++ frontend/src/routes/student/request/+page.svelte
@@ -1,48 +1,24 @@
 <script>
 	import { onMount } from 'svelte';
-	import { token } from '../../../stores';
+	import { token, course } from '../../../stores';
 	import Master from '../../../layouts/Master.svelte';
 	import { writable } from 'svelte/store';
 
 	let reason = '';
 	let error = '';
 	let success = '';
-	let courses = [];
-	let courseOfferingId = writable(null);
 	let passTypeId = writable("1");
 
 	onMount(async () => {
-		await fetchcourses();
+		
 	});
-
-	async function fetchcourses() {
-		try {
-			const storedToken = localStorage.getItem('token');
-			let token = JSON.parse(storedToken);
-
-			const response = await fetch('http://localhost:3100/api/courses', {
-				method: 'GET',
-				headers: {
-					Authorization: `Bearer ${token.access_token}`
-				}
-			});
-
-			if (response.ok) {
-				courses = await response.json();
-			} else {
-				const errorData = await response.json();
-				error = errorData.error;
-			}
-		} catch (err) {
-			error = 'An error occurred while fetching courses.';
-		}
-	}
 
 	async function submitFreePassRequest() {
 		try {
 			const storedToken = localStorage.getItem('token');
 			let token = JSON.parse(storedToken);
 
+			let courseOfferingId = $course.CourseOffering.id;
 			const response = await fetch('http://localhost:3100/api/freepassrequest', {
 				method: 'POST',
 				headers: {
@@ -78,12 +54,6 @@
 				required
 			/>
 			<div class="input-group-append">
-				<label for="">Course</label>
-				<select required class="form-control" name="course" bind:value={courseOfferingId}>
-					{#each courses as course}
-						<option value={course.CourseOffering.id}>{course.CourseOffering.Course.name} | {course.CourseOffering.Term.name}</option>
-					{/each}
-				</select>
 				<label for="">Pass Type</label>
 				<select required class="form-control" name="course" bind:value={passTypeId}>
 					<option value="1">Default Pass Type</option>
