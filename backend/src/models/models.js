ccee7f386defd47f15f49bc682a13dfffc42ba6a
Jharana Sapkota <jharanasapkota09@gmail.com>
2024-08-05 22:27:14 +0545

    Fix: single image for docker

--- backend/src/models/models.js
+++ backend/src/models/models.js
@@ -33,6 +33,8 @@ const userSchema = new mongoose.Schema({
 const clientSchema = new mongoose.Schema({
     clientId: String,
     clientSecret: String,
+    publicKey: String,
+    privateKey: String,
     grants: [String]
 });
 
